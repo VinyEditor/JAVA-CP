a11bd5d023fdb375bed7f884ce82eb6bbcb0a4f7
VinyEditor <111810869+VinyEditor@users.noreply.github.com>
2022-09-13 21:01:54 -0300

    Add files via upload
    Exercicios de JAVA

---
+++ Ex22.java
@@ -0,0 +1,19 @@
+package entrada_processamento_saida;
+
+import java.util.Scanner;
+
+public class Ex22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner ler = new Scanner (System.in);
+		/*22. Exibir o seguinte seletor de opções e em função de uma escolha, solicitar os dados necessários 
+		 * para o cálculo da respectiva área. Enviar mensagem de erro se o usuário escolher uma opção inexistente.
+		 * Encerrar o programa somente quando selecionada a opção de finalização. 
+		 * (Fazer esse exercício utilizando If..Else e/ou Case) 1 – Triângulo
+		 * 2 – Quadrado 3 – Retângulo 4 – Círculo 5 – Fim de processo */
+		
+		
+	}
+
+}
---
+++ Ex29.java
@@ -0,0 +1,13 @@
+package entrada_processamento_saida;
+
+public class ex29 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 /*29. Escreva um algoritmo que leia três valores inteiros e 
+		  * diferentes e mostre-os 
+		  * em ordem decrescente.
+		  */
+	}
+
+}
